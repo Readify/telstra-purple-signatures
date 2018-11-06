24c327da9dc5a0f485027abf313d9f857774eb87
Jonathan Van Buren <vanbujm@gmail.com>
2018-11-06 11:43:31 +0800

    Made the instructions fool-proof

--- app.js
+++ app.js
@@ -300,10 +300,21 @@ class Info extends React.Component {
         <li>Open outlook</li>
         <li>
           <a
+            style={{ fontWeight: 'bold', textDecoration: 'underline' }}
             href="https://support.office.com/en-us/article/create-and-add-a-signature-to-messages-8ee5d4f4-68fd-464a-a1c1-0e1c80bb27f2"
             target="_blank">
             Paste selection into signature editor
           </a>
+          <ul style={{ paddingLeft: '1rem', listStyleType: 'none', paddingTop: '0.5rem' }}>
+            <li style={{marginBottom: '0.5rem'}}>
+              <span style={{ fontWeight: 'bold' }}>Windows: </span>
+              Signature > Signatures from the Message menu
+            </li>
+            <li>
+              <span style={{ fontWeight: 'bold' }}>Mac: </span>
+              Preferences > Signatures from the Message menu
+            </li>
+          </ul>
         </li>
       </ol>
       <br/>
