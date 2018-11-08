9d8bae9616119b52d6d82b48169add1598d5841b
Jonathan Van Buren <vanbujm@gmail.com>
2018-11-08 15:03:51 +0800

    config fixing6

---
+++ public/git-askpass-helper.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec echo "$AZURE_PASSWORD"
--- public/publish.sh
+++ public/publish.sh
@@ -3,7 +3,7 @@ git config --global user.email "$AZURE_EMAIL"
 git config --global user.name "$AZURE_USERNAME"
 
 git init
-git remote add origin "https://$AZURE_USERNAME:$AZURE_PASSWORD@signature-generator.scm.azurewebsites.net:443/signature-generator.git"
+git remote add origin "https://$AZURE_USERNAME@signature-generator.scm.azurewebsites.net:443/signature-generator.git"
 git add -A
 git commit -m "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
 git push --force --set-upstream origin master
