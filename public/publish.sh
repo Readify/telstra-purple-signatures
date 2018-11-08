b1e851c08f905b1dd5c5ef686f4ccd094716b04c
Jonathan Van Buren <vanbujm@gmail.com>
2018-11-08 14:53:58 +0800

    config fixing5

--- public/publish.sh
+++ public/publish.sh
@@ -4,5 +4,6 @@ git config --global user.name "$AZURE_USERNAME"
 
 git init
 git remote add origin "https://$AZURE_USERNAME:$AZURE_PASSWORD@signature-generator.scm.azurewebsites.net:443/signature-generator.git"
-git commit -am "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
+git add -A
+git commit -m "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
 git push --force --set-upstream origin master
