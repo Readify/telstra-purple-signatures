097f70c6e2c30fc294bf1c1ce4879f7790a01cd7
Jonathan Van Buren <vanbujm@gmail.com>
2018-11-13 09:51:33 +0800

    \fixed nav

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,10 +3,16 @@ import './Header.scss';
 
 const Header = ({ brandInfo }) => (
   <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a className="navbar-item" href={brandInfo.brandLink}>
-        {brandInfo.appName}
-      </a>
+    <div className="columns" style={{ width: '100%' }}>
+      <div className="column" />
+      <div className="column is-three-quarters">
+        <div className="navbar-brand">
+          <a className="navbar-item" href={brandInfo.brandLink}>
+            {brandInfo.appName}
+          </a>
+        </div>
+      </div>
+      <div className="column" />
     </div>
   </nav>
 );
