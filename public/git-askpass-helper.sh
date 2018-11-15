71dcaab3da36a587f2d0ca22bbe66a1235863803
Jonathan Van Buren <vanbujm@gmail.com>
2018-11-15 10:57:05 +0800

    Removed unused scripts from public directory

--- public/git-askpass-helper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec echo "$AZURE_PASSWORD"
--- public/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git config --global user.email "$AZURE_EMAIL"
-git config --global user.name "$AZURE_USERNAME"
-
-git init
-git remote add origin "https://$AZURE_USERNAME@signature-generator.scm.azurewebsites.net:443/signature-generator.git"
-git add -A
-git commit -m "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
-git push --force --set-upstream origin master
