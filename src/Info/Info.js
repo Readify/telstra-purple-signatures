475ea434804c2d71d262e554a67f8502b3da1d47
Adrian Clark <adrian.clark@readify.net>
2019-09-16 16:58:26 +1000

    Revert escaping quotes
    This wasn't the right thing to do.

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -36,7 +36,7 @@ const Info = () => (
             <span style={{ fontWeight: 'bold' }}>
               Outlook web access (OWA):{' '}
             </span>
-            Cog (top-right) > &amp;quot;View all Outlook settings&amp;quot; link (bottom) > Mail > Compose and reply > Email signature
+            Cog (top-right) > "View all Outlook settings" link (bottom) > Mail > Compose and reply > Email signature
           </li>
         </ul>
       </li>
