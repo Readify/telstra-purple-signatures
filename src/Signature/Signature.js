5dff2a40ea74d23cefea7ec3b99a0e46125a7eee
Rob Moore <rob.moore@readify.net>
2019-09-27 09:36:39 +0800

    Removing changes from ad43d1b4b5e694792ff6aca21fc0173392f48406 to Signature.js that added <tbody> since that breaks email clients

--- src/Signature/Signature.js
+++ src/Signature/Signature.js
@@ -32,44 +32,115 @@ const Signature = props => {
           width: '500px'
         }}
       >
-        <tbody>
-          <tr>
-            <td valign="top">
-              {/*Offset text so it lines up with top of logo on light mode UIs and works in Outlook*/}
-              <div style={{ height: '15px', lineHeight: '15px' }}>&nbsp;</div>
-              <p>
-                <b>{name}</b>
-                <br />
-                {brandName}
-                <br />
-                <span style={{ color: '#A5A5A5' }}>{title}</span>
-                <br />
-                {qualifications ? (
-                  <span>
-                    {qualifications}
-                    <br />
-                  </span>
-                ) : null}
-              </p>
-            </td>
-            <td
-              valign="top"
+        <tr>
+          <td valign="top">
+            {/*Offset text so it lines up with top of logo on light mode UIs and works in Outlook*/}
+            <div style={{ height: '15px', lineHeight: '15px' }}>&nbsp;</div>
+            <p>
+              <b>{name}</b>
+              <br />
+              {brandName}
+              <br />
+              <span style={{ color: '#A5A5A5' }}>{title}</span>
+              <br />
+              {qualifications ? (
+                <span>
+                  {qualifications}
+                  <br />
+                </span>
+              ) : null}
+            </p>
+          </td>
+          <td
+            valign="top"
+            style={{
+              paddingRight: '20px',
+              width: '96px'
+            }}
+          >
+            <img
+              width="83"
+              height="83"
+              style={{ width: '83px', height: '83px', maxWidth: 'none' }}
+              src={brandLogo.link}
+              alt={brandLogo.alt}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table
+              border="0"
+              cellSpacing="0"
+              cellPadding="0"
               style={{
-                paddingRight: '20px',
-                width: '96px'
+                color: 'black',
+                fontFamily: 'Arial, sans-serif',
+                fontSize: '9pt',
+                margin: 0,
+                padding: 0
               }}
             >
-              <img
-                width="83"
-                height="83"
-                style={{ width: '83px', height: '83px', maxWidth: 'none' }}
-                src={brandLogo.link}
-                alt={brandLogo.alt}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
+              <tr>
+                <td style={{ padding: 0, width: '30px' }}>
+                  <b>M</b>
+                </td>
+                <td style={{ padding: 0 }}>
+                  <a
+                    style={{ color: brandInfo.brandLinkColour }}
+                    href={`tel:${parseMobile(mobile).replace(/\s/g, '')}`}
+                    dangerouslySetInnerHTML={{
+                      __html: parseMobile(mobile).replace(/\s/g, '&nbsp;')
+                    }}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td style={{ padding: 0 }}>
+                  <b>E</b>
+                </td>
+                <td style={{ padding: 0 }}>
+                  <a
+                    href={`mailto:${email}`}
+                    style={{ color: brandInfo.brandLinkColour }}
+                  >
+                    {email}
+                  </a>
+                </td>
+              </tr>
+              {twitter ? (
+                <tr>
+                  <td style={{ padding: 0 }}>
+                    <b>T</b>
+                  </td>
+                  <td style={{ padding: 0 }}>
+                    <a
+                      href={`https://twitter.com/${twitter.replace('@', '')}`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      style={{ color: brandInfo.brandLinkColour }}
+                    >
+                      @{twitter.replace('@', '')}
+                    </a>
+                  </td>
+                </tr>
+              ) : null}
+              <tr>
+                <td style={{ padding: 0 }}>
+                  <b>W</b>
+                </td>
+                <td style={{ padding: 0 }}>
+                  <a
+                    href={brandLink}
+                    style={{ color: brandInfo.brandLinkColour }}
+                  >
+                    {brandLinkName}
+                  </a>
+                </td>
+              </tr>
+            </table>
+
+            {isSupport ? (
               <table
                 border="0"
                 cellSpacing="0"
@@ -82,134 +153,53 @@ const Signature = props => {
                   padding: 0
                 }}
               >
-                <tbody>
-                  <tr>
-                    <td style={{ padding: 0, width: '30px' }}>
-                      <b>M</b>
-                    </td>
-                    <td style={{ padding: 0 }}>
-                      <a
-                        style={{ color: brandInfo.brandLinkColour }}
-                        href={`tel:${parseMobile(mobile).replace(/\s/g, '')}`}
-                        dangerouslySetInnerHTML={{
-                          __html: parseMobile(mobile).replace(/\s/g, '&nbsp;')
-                        }}
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style={{ padding: 0 }}>
-                      <b>E</b>
-                    </td>
-                    <td style={{ padding: 0 }}>
-                      <a
-                        href={`mailto:${email}`}
-                        style={{ color: brandInfo.brandLinkColour }}
-                      >
-                        {email}
-                      </a>
-                    </td>
-                  </tr>
-                  {twitter ? (
-                    <tr>
-                      <td style={{ padding: 0 }}>
-                        <b>T</b>
-                      </td>
-                      <td style={{ padding: 0 }}>
-                        <a
-                          href={`https://twitter.com/${twitter.replace(
-                            '@',
-                            ''
-                          )}`}
-                          rel="noopener noreferrer"
-                          target="_blank"
-                          style={{ color: brandInfo.brandLinkColour }}
-                        >
-                          @{twitter.replace('@', '')}
-                        </a>
-                      </td>
-                    </tr>
-                  ) : null}
-                  <tr>
-                    <td style={{ padding: 0 }}>
-                      <b>W</b>
-                    </td>
-                    <td style={{ padding: 0 }}>
-                      <a
-                        href={brandLink}
-                        style={{ color: brandInfo.brandLinkColour }}
-                      >
-                        {brandLinkName}
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
+                <tr>
+                  <td style={{ padding: 0, width: '100px' }}>
+                    <b>Support&nbsp;Hotline</b>
+                  </td>
+                  <td style={{ padding: 0 }}>
+                    <a
+                      href={`tel:${parseLandLine(supportHotline).replace(
+                        /&nbsp;/g,
+                        ''
+                      )}`}
+                      dangerouslySetInnerHTML={{
+                        __html: parseLandLine(supportHotline)
+                      }}
+                      style={{ color: brandInfo.brandLinkColour }}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ padding: 0 }}>
+                    <b>Support&nbsp;Email</b>
+                  </td>
+                  <td style={{ padding: 0 }}>
+                    <a
+                      href={`mailto:${supportEmail}`}
+                      style={{ color: brandInfo.brandLinkColour }}
+                    >
+                      {supportEmail}
+                    </a>
+                  </td>
+                </tr>
               </table>
-
-              {isSupport ? (
-                <table
-                  border="0"
-                  cellSpacing="0"
-                  cellPadding="0"
-                  style={{
-                    color: 'black',
-                    fontFamily: 'Arial, sans-serif',
-                    fontSize: '9pt',
-                    width: '500px',
-                    margin: 0,
-                    padding: 0
-                  }}
-                >
-                  <tbody>
-                    <tr>
-                      <td style={{ padding: 0, width: '100px' }}>
-                        <b>Support&nbsp;Hotline</b>
-                      </td>
-                      <td style={{ padding: 0 }}>
-                        <a
-                          href={`tel:${parseLandLine(supportHotline).replace(
-                            /&nbsp;/g,
-                            ''
-                          )}`}
-                          dangerouslySetInnerHTML={{
-                            __html: parseLandLine(supportHotline)
-                          }}
-                          style={{ color: brandInfo.brandLinkColour }}
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td style={{ padding: 0 }}>
-                        <b>Support&nbsp;Email</b>
-                      </td>
-                      <td style={{ padding: 0 }}>
-                        <a
-                          href={`mailto:${supportEmail}`}
-                          style={{ color: brandInfo.brandLinkColour }}
-                        >
-                          {supportEmail}
-                        </a>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              ) : null}
-            </td>
-          </tr>
-          <tr>
-            <td
-              style={{
-                color: 'black',
-                fontFamily: 'Arial, sans-serif',
-                fontSize: '6.5pt'
-              }}
-            >
-              This email may contain confidential information.
-              <br />
-              If I've sent it to you by accident, please delete it immediately.
-            </td>
-          </tr>
-        </tbody>
+            ) : null}
+          </td>
+        </tr>
+        <tr>
+          <td
+            style={{
+              color: 'black',
+              fontFamily: 'Arial, sans-serif',
+              fontSize: '6.5pt'
+            }}
+          >
+            This email may contain confidential information.
+            <br />
+            If I've sent it to you by accident, please delete it immediately.
+          </td>
+        </tr>
       </table>
     </div>
   );
